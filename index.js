55effab37cb746ee31d134a5cac1d9b527366d16
jonschlinkert <jon.schlinkert@sellside.com>
2016-06-08 21:50:48 -0400

    first release

--- index.js
+++ index.js
@@ -1,4 +1,49 @@
 'use strict';
 
-module.exports = function() {
+var isValid = require('is-valid-app');
+var merge = require('mixin-deep');
+
+/**
+ * Expose `context` plugin
+ */
+
+module.exports = function(view) {
+  if (isValid(view, 'view-context', ['view', 'item', 'file'])) {
+    this.define('context', calculate);
+  }
 };
+
+/**
+ * Expose `calculate` function for direct usage
+ */
+
+module.exports.calculate = calculate;
+
+/**
+ * Calculate context with the given locals
+ */
+
+function calculate(locals) {
+  if (this._context) return this._context;
+
+  if (typeof locals === 'function') {
+    return locals.call(this, this);
+  }
+  if (arguments.length > 1) {
+    locals = [].concat.apply([], [].slice.call(arguments));
+  } else {
+    locals = arrayify(locals);
+  }
+
+  locals.unshift(this.locals);
+  locals.push(this.data);
+  return merge.apply(merge, locals);
+}
+
+/**
+ * Arrayify the given value by casting it to an array.
+ */
+
+function arrayify(val) {
+  return val ? (Array.isArray(val) ? val : [val]) : [];
+}
--- test.js
+++ test.js
@@ -2,27 +2,53 @@
 
 require('mocha');
 var assert = require('assert');
+var View = require('vinyl-view');
 var context = require('./');
+var view;
 
 describe('view-context', function() {
+  beforeEach(function() {
+    view = new View();
+    // view.use(context);
+  });
+
   it('should export a function', function() {
     assert.equal(typeof context, 'function');
   });
 
-  it('should export an object', function() {
-    assert(context);
-    assert.equal(typeof context, 'object');
+  it('should create context using the given locals object', function() {
+    var ctx = view.context({foo: 'bar'});
+    assert.deepEqual(ctx, {foo: 'bar'});
   });
 
-  it('should throw an error when invalid args are passed', function(cb) {
-    try {
-      context();
-      cb(new Error('expected an error'));
-    } catch (err) {
-      assert(err);
-      assert.equal(err.message, 'expected first argument to be a string');
-      assert.equal(err.message, 'expected callback to be a function');
-      cb();
-    }
+  it('should create context using view.locals', function() {
+    view.locals = {foo: 'bar'};
+    var ctx = view.context();
+    assert.deepEqual(ctx, {foo: 'bar'});
+  });
+
+  it('should create context using view.data', function() {
+    view.data = {foo: 'bar'};
+    var ctx = view.context();
+    assert.deepEqual(ctx, {foo: 'bar'});
+  });
+
+  it('should use locals over view.locals', function() {
+    view.locals = {foo: 'bar'};
+    var ctx = view.context({foo: 'baz'});
+    assert.deepEqual(ctx, {foo: 'baz'});
+  });
+
+  it('should use locals over view.data', function() {
+    view.data = {foo: 'bar'};
+    var ctx = view.context({foo: 'baz'});
+    assert.deepEqual(ctx, {foo: 'baz'});
+  });
+
+  it('should use `view.data` over `view.locals`', function() {
+    view = new View({data: {foo: 'bar'}, locals: {foo: 'baz'}});
+    view.use(context);
+    var ctx = view.context();
+    assert.deepEqual(ctx, {foo: 'bar'});
   });
 });
